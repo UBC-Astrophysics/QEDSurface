b5f2f1d978b7b92c9e5104b3e4b3226303eb67ef
Jeremy Heyl <heyl@phas.ubc.ca>
2016-06-08 22:06:14 -0700

    updated plots and scripts

--- results_ns/ns_radius.sh
+++ results_ns/ns_radius.sh
@@ -1,4 +1,13 @@
-ctioga2 --name ns_radius \
-	cone_summary_6.1@'(if $5>0 && $2==0.5e18 && $4==1e6 then $5 else 0.0/0.0 end):-$6/$7' \
-	@'(if $5>0 && $2==1e18 && $4==1e6 then $5 else 0.0/0.0 end):-$6/$7' \
-	@'(if $5>0 && $2==2e18 && $4==1e6 then $5 else 0.0/0.0 end):-$6/$7' 
+for ang in 15 30 45 60 75
+do
+    ctioga2 --name ns_radius_${ang} \
+	    --xfact 4.14e-18 \
+	    --legend '10 km' \
+	    cone_summary_6.1@"(if \$5==${ang} && \$4==1e6 then \$2 else 0.0/0.0 end):-\$6/\$7" \
+	    --legend '12 km' \
+	    @"(if \$5==${ang} && \$4==1.2e6 then \$2 else 0.0/0.0 end):-\$6/\$7" \
+	    --legend '14 km' \
+	    @"(if \$5==30 && \$4==1.4e6 then \$2 else 0.0/0.0 end):-\$6/\$7" \
+	    -x 'Energy [keV]' -y "Polarized Fraction (${ang} degrees)"
+done
+
