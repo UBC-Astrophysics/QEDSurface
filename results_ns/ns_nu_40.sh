b98c82c92b789d3a0614aa9e2708169f0fd5c5b2
Jeremy Heyl <heyl@phas.ubc.ca>
2016-06-08 22:07:45 -0700

    removed ns_nu

--- results_ns/ns_nu_40.sh
+++
@@ -1,9 +0,0 @@
-ctioga2 --name ns_nu_40 \
-	--xfact 4.14e-18 \
-	--legend '10 km' \
-	cone_summary_6.1@'(if $5==40 && $4==1e6 then $2 else 0.0/0.0 end):-$6/$7' \
-	--legend '12 km' \
-	@'(if $5==40 && $4==1.2e6 then $2 else 0.0/0.0 end):-$6/$7' \
-	--legend '14 km' \
-	@'(if $5==40 && $4==1.4e6 then $2 else 0.0/0.0 end):-$6/$7' \
-	-x 'Energy [keV]' -y 'QED Decrement at 40 degrees'
